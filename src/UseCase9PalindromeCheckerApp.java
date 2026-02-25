bf73aed9c076c7c53454e9f81bf8ba9541ec48fe
SuchithaKundur <sk7229@srmist.edu.in>
2026-02-25 15:37:13 +0530

    UC9: Recursive Palindrome Checker

---
+++ src/UseCase9PalindromeCheckerApp.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class UseCase9PalindromeCheckerApp {
+    public static boolean isPalindrome(String str, int start, int end) {
+
+        if (start >= end) {
+            return true;
+        }
+
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
+        }
+        return isPalindrome(str, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("===== Recursive Palindrome Checker App =====");
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        String processedInput = input.replaceAll("\\s+", "").toLowerCase();
+
+        boolean result = isPalindrome(processedInput, 0, processedInput.length() - 1);
+
+        if (result) {
+            System.out.println("Result: The given string is a Palindrome.");
+        } else {
+            System.out.println("Result: The given string is NOT a Palindrome.");
+        }
+
+        scanner.close();
+    }
+}
